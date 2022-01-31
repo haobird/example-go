1aaf6151215d528dcd79c48302b2d2922f19a812
jiebaolong <jiebaolong@ctfo.com>
2022-01-31 15:33:48 +0800

    增加新的功能

---
+++ hellogrpc/client/main.go
@@ -0,0 +1,38 @@
+package main
+
+import (
+	"context"
+	"flag"
+	"log"
+	"time"
+
+	pb "github.com/haobird/learngo/hellogrpc/message"
+	"google.golang.org/grpc"
+)
+
+const (
+	defaultName = "world"
+)
+
+var (
+	addr = flag.String("addr", "localhost:50051", "the address to connect to")
+	name = flag.String("name", defaultName, "name to greet")
+)
+
+func main() {
+	flag.Parse()
+	conn, err := grpc.Dial(*addr, grpc.WithInsecure())
+	if err != nil {
+		log.Fatalf("did not connect: %v", err)
+	}
+	defer conn.Close()
+	c := pb.NewGreeterClient(conn)
+
+	ctx, cancel := context.WithTimeout(context.Background(), time.Second)
+	defer cancel()
+	r, err := c.SayHello(ctx, &pb.HelloRequest{Name: *name})
+	if err != nil {
+		log.Fatalf("could not greet: %v", err)
+	}
+	log.Printf("Greeting: %s", r.GetMessage())
+}
---
+++ hellogrpc/go.mod
@@ -0,0 +1,9 @@
+module github.com/haobird/learngo/hellogrpc
+
+go 1.16
+
+require (
+	github.com/golang/protobuf v1.5.2
+	google.golang.org/grpc v1.44.0
+	google.golang.org/protobuf v1.27.1
+)
